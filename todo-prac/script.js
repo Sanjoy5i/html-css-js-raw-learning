e98e66a2070dd7ff18df9b566088344f27f2f511
sanjoy-git <sanjoyroy66@outlook.com>
2023-06-18 20:15:52 +0600

    commit3

--- todo-prac/script.js
+++ todo-prac/script.js
@@ -1,3 +1,20 @@
-setTimeout(() => {
-  document.getElementsByTagName('h1')[0].innerHTML="Thank you!"
-}, 1000);
+const text = document.getElementById('text');
+const display = document.getElementById('display');
+
+
+document.getElementById('add').addEventListener('click',()=>{
+  if(text.value){
+    const p = document.createElement('p');
+    p.innerHTML = text.value + "<b style='color:red; cursor:pointer'> &#9003</b>";
+    p.style.fontSize='20pt'
+    display.appendChild(p);
+    p.addEventListener('click',()=>{
+      display.removeChild(p);
+    })
+  }
+  else{
+    alert("please enter text.");
+  }
+});
+
+// display.innerHTML="<i>&#9003</i>"
